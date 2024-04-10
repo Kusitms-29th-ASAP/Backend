97c1b53ff2d2c5185004793c61dd77935ca0aac4
Sim-km <simkm0824@naver.com>
2024-04-10 14:44:40 +0900

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,58 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.springframework.boot") version "3.2.4"
+    id("io.spring.dependency-management") version "1.1.4"
+    kotlin("jvm") version "1.9.23"
+    kotlin("plugin.spring") version "1.9.23"
+    kotlin("plugin.jpa") version "1.9.23"
+}
+
+group = "com.asap"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+    mavenCentral()
+}
+
+val kotestVersion = "5.8.1"
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+
+    // jpa
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    runtimeOnly("com.mysql:mysql-connector-j")
+
+    // security
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    testImplementation("org.springframework.security:spring-security-test")
+
+    // kotest
+    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")  // Test Framework
+    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion") // Assertions Library
+    testImplementation("io.kotest:kotest-property:$kotestVersion") //
+
+    // fixture monkey
+    testImplementation("com.navercorp.fixturemonkey:fixture-monkey-starter:1.0.14")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs += "-Xjsr305=strict"
+        jvmTarget = "17"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ src/main/kotlin/com/asap/asapbackend/AsapBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.asap.asapbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AsapBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<AsapBackendApplication>(*args)
+}
