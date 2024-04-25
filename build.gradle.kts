5e8676f4b8111aca99ab5f47dde3eaca475e6eca
Sim-km <simkm0824@naver.com>
2024-04-25 20:01:22 +0900

    chore: rest docs 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,17 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+    `java-test-fixtures`
+
     id("org.springframework.boot") version "3.2.4"
     id("io.spring.dependency-management") version "1.1.4"
+
+    id("org.asciidoctor.jvm.convert") version "4.0.2"
+
     kotlin("jvm") version "1.9.23"
     kotlin("plugin.spring") version "1.9.23"
     kotlin("plugin.jpa") version "1.9.23"
+
 }
 
 group = "com.asap"
@@ -20,14 +26,25 @@ repositories {
 }
 
 val kotestVersion = "5.8.1"
+val mockkVersion = "1.13.10"
+
+
+val asciidoctorExt = "asciidoctorExt"
+configurations.create(asciidoctorExt) {
+    extendsFrom(configurations.testImplementation.get())
+}
+
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
 
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    // spring test
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testFixturesImplementation("org.springframework.boot:spring-boot-starter-test")
 
     // jpa
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
@@ -42,8 +59,25 @@ dependencies {
     testImplementation("io.kotest:kotest-assertions-core:$kotestVersion") // Assertions Library
     testImplementation("io.kotest:kotest-property:$kotestVersion") //
 
+    testImplementation("io.mockk:mockk:${mockkVersion}") // mockk
+
     // fixture monkey
-    testImplementation("com.navercorp.fixturemonkey:fixture-monkey-starter:1.0.14")
+    testImplementation("com.navercorp.fixturemonkey:fixture-monkey-starter-kotlin:1.0.16")
+    testFixturesImplementation("com.navercorp.fixturemonkey:fixture-monkey-starter-kotlin:1.0.16")
+
+    // JWT
+    implementation("io.jsonwebtoken:jjwt-api:0.12.5")
+    implementation("io.jsonwebtoken:jjwt-impl:0.12.5")
+    implementation("io.jsonwebtoken:jjwt-jackson:0.12.5")
+
+    // caffeine
+    implementation("com.github.ben-manes.caffeine:caffeine:3.1.8")
+
+
+    // RestDocs
+    testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
+    asciidoctorExt("org.springframework.restdocs:spring-restdocs-asciidoctor")
+    testFixturesImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
 }
 
 tasks.withType<KotlinCompile> {
@@ -53,6 +87,33 @@ tasks.withType<KotlinCompile> {
     }
 }
 
-tasks.withType<Test> {
+val snippetDir = file("build/generated-snippets")
+
+tasks.test {
+    outputs.dir(snippetDir)
     useJUnitPlatform()
+}
+
+tasks.asciidoctor {
+    inputs.dir(snippetDir)
+    dependsOn(tasks.test)
+    configurations(asciidoctorExt)
+//    baseDirFollowsSourceFile()
+}
+
+tasks.register<Copy>("copyDocument") {
+    dependsOn(tasks.asciidoctor)
+    val docsDir = file("src/main/resources/static/docs")
+    val fromDir = file("build/docs/asciidoc")
+    doFirst {
+        if (docsDir.exists())
+            delete(docsDir)
+    }
+    from(fromDir)
+    into(docsDir)
+
+}
+
+tasks.build {
+    dependsOn(tasks.getByName("copyDocument"))
 }
