bfc06031e4ce3680404e2a31f553a93b777162e5
Sim-km <simkm0824@naver.com>
2024-04-27 16:28:24 +0900

    refactor: jwt 패키지 이동으로 테스트 클래스 이동

--- src/test/kotlin/com/asap/asapbackend/global/jwt/JwtProviderTest.kt
+++ src/test/kotlin/com/asap/asapbackend/global/jwt/JwtProviderTest.kt
@@ -1,4 +1,4 @@
-package com.asap.asapbackend.global.security.jwt
+package com.asap.asapbackend.global.jwt
 
 import com.asap.asapbackend.fixture.generateFixture
 import com.asap.asapbackend.generateBasicParser
