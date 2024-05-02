e5f240572fe27a5aa5bd7768509c4ba13423bf1b
Somin Yun <somin455@naver.com>
2024-05-03 07:31:05 +0900

    fix: #24 교육청코드, 학교코드 추가

--- src/main/kotlin/com/asap/asapbackend/domain/school/domain/model/School.kt
+++ src/main/kotlin/com/asap/asapbackend/domain/school/domain/model/School.kt
@@ -5,9 +5,11 @@ import jakarta.persistence.Entity
 
 @Entity
 class School(
-    val name: String,
-    val address: String
-):BaseDateEntity() {
+        val eduOfiiceCode: String,
+        val schoolCode: String,
+        val name: String,
+        val address: String
+) : BaseDateEntity() {
     init {
         require(name.isNotBlank()) { "학교 이름은 빈 값이 될 수 없습니다." }
         require(address.isNotBlank()) { "주소는 빈 값이 될 수 없습니다." }
