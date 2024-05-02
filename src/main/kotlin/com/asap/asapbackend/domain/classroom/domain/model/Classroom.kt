e145357087fd99090ce7bdd74f3234a3caa1d0ee
Sim-km <simkm0824@naver.com>
2024-05-03 01:19:39 +0900

    feat: #25 학년반과 학교와 연관 관계 추가

--- src/main/kotlin/com/asap/asapbackend/domain/classroom/domain/model/Classroom.kt
+++ src/main/kotlin/com/asap/asapbackend/domain/classroom/domain/model/Classroom.kt
@@ -1,13 +1,18 @@
 package com.asap.asapbackend.domain.classroom.domain.model
 
+import com.asap.asapbackend.domain.school.domain.model.School
 import com.asap.asapbackend.global.domain.BaseDateEntity
 import jakarta.persistence.Embedded
 import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.ManyToOne
 
 @Entity
 class Classroom(
     val garde: Grade,
     val classNumber: Int,
     @Embedded
-    val year: Year
+    val year: Year,
+    @ManyToOne(fetch = FetchType.LAZY)
+    val school: School
 ): BaseDateEntity()
