7f8df5bce45e18373bd1aeec9186744197edba14
Sim-km <simkm0824@naver.com>
2024-05-03 16:53:39 +0900

    feat: #24 사용자 저장 애플리케이션 서비스, 도메인 서비스 추가

---
+++ src/main/kotlin/com/asap/asapbackend/domain/user/application/UserService.kt
@@ -0,0 +1,45 @@
+package com.asap.asapbackend.domain.user.application
+
+import com.asap.asapbackend.domain.child.domain.service.ChildAppender
+import com.asap.asapbackend.domain.classroom.domain.service.ClassroomAppender
+import com.asap.asapbackend.domain.classroom.domain.service.ClassroomReader
+import com.asap.asapbackend.domain.user.application.dto.CreateUser
+import com.asap.asapbackend.domain.user.domain.service.UserAppender
+import com.asap.asapbackend.global.jwt.Claims
+import com.asap.asapbackend.global.jwt.JwtProvider
+import com.asap.asapbackend.global.jwt.TokenExtractor
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+@Transactional(readOnly = true)
+class UserService(
+    private val jwtProvider: JwtProvider,
+    private val tokenExtractor: TokenExtractor,
+    private val userAppender: UserAppender,
+    private val classroomReader: ClassroomReader,
+    private val classroomAppender: ClassroomAppender,
+    private val childAppender: ChildAppender,
+) {
+
+    @Transactional
+    fun createUser(request: CreateUser.Request): CreateUser.Response {
+        val user = request.extractUser {
+            val registrationToken = tokenExtractor.extractValue(it)
+            jwtProvider.extractRegistrationClaimsFromToken(registrationToken)
+        }.apply { userAppender.appendUser(this) }
+
+        request.children.map {
+            val classroom = it.extractClassroom { grade, classNumber, schoolId ->
+                classroomReader.findByGradeAndClassNumberAndSchoolId(grade, classNumber, schoolId)
+            }
+            val child = it.extractChild(user).apply { childAppender.appendChild(this) }
+            classroomAppender.addChild(classroom, child)
+        }
+        val userClaims = Claims.UserClaims(user.id)
+        return CreateUser.Response(
+            jwtProvider.generateAccessToken(userClaims),
+            jwtProvider.generateRefreshToken(userClaims)
+        )
+    }
+}
