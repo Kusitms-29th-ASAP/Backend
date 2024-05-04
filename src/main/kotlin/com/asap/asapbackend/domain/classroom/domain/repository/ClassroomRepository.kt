138b5279db6e311c1b17b39e299948670095f357
Sim-km <simkm0824@naver.com>
2024-05-04 09:39:52 +0900

    fix: 누락된 의존성 추가

--- src/main/kotlin/com/asap/asapbackend/domain/classroom/domain/repository/ClassroomRepository.kt
+++ src/main/kotlin/com/asap/asapbackend/domain/classroom/domain/repository/ClassroomRepository.kt
@@ -1,6 +1,8 @@
 package com.asap.asapbackend.domain.classroom.domain.repository
 
 import com.asap.asapbackend.domain.classroom.domain.model.Classroom
+import com.asap.asapbackend.domain.classroom.domain.model.Grade
+import com.asap.asapbackend.domain.school.domain.model.School
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface ClassroomRepository: JpaRepository<Classroom, Long> {
--- src/main/kotlin/com/asap/asapbackend/domain/classroom/domain/service/ClassroomAppender.kt
+++ src/main/kotlin/com/asap/asapbackend/domain/classroom/domain/service/ClassroomAppender.kt
@@ -1,6 +1,12 @@
 package com.asap.asapbackend.domain.classroom.domain.service
 
+import com.asap.asapbackend.batch.classroom.model.ClassroomResponse
+import com.asap.asapbackend.domain.child.domain.model.Child
+import com.asap.asapbackend.domain.classroom.domain.model.ChildClassroom
 import com.asap.asapbackend.domain.classroom.domain.model.Classroom
+import com.asap.asapbackend.domain.classroom.domain.model.Grade
+import com.asap.asapbackend.domain.classroom.domain.model.Year
+import com.asap.asapbackend.domain.classroom.domain.repository.ChildClassRoomRepository
 import com.asap.asapbackend.domain.classroom.domain.repository.ClassroomRepository
 import org.springframework.stereotype.Service
 
@@ -17,13 +23,14 @@ class ClassroomAppender(
                 student = student
             )
         ).id
+    }
     fun addClassroom(classrooms: List<ClassroomResponse>) {
         for (classroom in classrooms) {
             if(classroomRepository.findBySchoolAndGradeAndClassNumber(classroom.school, Grade.convert(classroom.grade),classroom.classNumber)==null){
                 val newClassroom = Classroom(
                         grade = classroom.grade,
                         classNumber = classroom.classNumber,
-                        year = year(),
+                        year = Year(),
                         school = classroom.school
                 )
                 classroomRepository.save(newClassroom)
