1e914895a853af36f241bbe1f478a73bfbf4a111
Sim-km <simkm0824@naver.com>
2024-05-06 19:48:21 +0900

    chore: rest docs 설정 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -119,16 +119,17 @@ tasks.asciidoctor {
 
 tasks.bootJar{
     dependsOn(tasks.asciidoctor)
-    copy{
-        from("build/docs/asciidoc")
-        into("src/main/resources/static/docs")
-    }
     from("build/docs/asciidoc"){
         into("static/docs")
     }
 }
 
-
-tasks.build{
+tasks.register("copyDocs", Copy::class){
     dependsOn(tasks.bootJar)
+    from("build/docs/asciidoc")
+    into("src/main/resources/static/docs")
+}
+
+tasks.build {
+    dependsOn(tasks.getByName("copyDocs"))
 }
