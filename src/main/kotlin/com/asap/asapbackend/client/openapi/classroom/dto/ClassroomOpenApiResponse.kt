0750850b29ccc43f482286907d10726b10714738
sominyun <somin455@naver.com>
2024-05-07 23:24:29 +0900

    fix: #39 api response 필요없는 경우 제거

--- src/main/kotlin/com/asap/asapbackend/client/openapi/classroom/dto/ClassroomOpenApiResponse.kt
+++ src/main/kotlin/com/asap/asapbackend/client/openapi/classroom/dto/ClassroomOpenApiResponse.kt
@@ -24,18 +24,8 @@ data class Result(
 )
 
 data class Row(
-    val ATPT_OFCDC_SC_CODE: String?,
-    val ATPT_OFCDC_SC_NM: String?,
-    val SD_SCHUL_CODE: String?,
-    val SCHUL_NM: String?,
-    val AY: String?,
     val GRADE: Int,
-    val DGHT_CRSE_SC_NM: String?,
-    val SCHUL_CRSE_SC_NM: String?,
-    val ORD_SC_NM: String?,
-    val DDDEP_NM: String?,
-    val CLASS_NM: String,
-    val LOAD_DTM: String?
+    val CLASS_NM: String
 ) {
     fun toClassroomInfo(school: School): ClassroomInfoProvider.ClassroomInfo {
         return ClassroomInfoProvider.ClassroomInfo(
