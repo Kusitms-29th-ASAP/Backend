d861498cf0169c5a2550eba8ae4babb94c29da8c
Sim-km <simkm0824@naver.com>
2024-05-13 15:41:51 +0900

    feat: #50 자녀 생성시 대표 자녀를 저장하도록 추가

--- src/main/kotlin/com/asap/asapbackend/domain/child/domain/service/ChildAppender.kt
+++ src/main/kotlin/com/asap/asapbackend/domain/child/domain/service/ChildAppender.kt
@@ -1,14 +1,20 @@
 package com.asap.asapbackend.domain.child.domain.service
 
 import com.asap.asapbackend.domain.child.domain.model.Child
+import com.asap.asapbackend.domain.child.domain.model.PrimaryChild
 import com.asap.asapbackend.domain.child.domain.repository.ChildRepository
+import com.asap.asapbackend.domain.child.domain.repository.PrimaryChildRepository
 import org.springframework.stereotype.Service
 
 @Service
 class ChildAppender(
-    private val childRepository: ChildRepository
+    private val childRepository: ChildRepository,
+    private val primaryChildRepository: PrimaryChildRepository
 ) {
     fun appendChild(child: Child) {
+        if(primaryChildRepository.existsByUserId(child.parent.id).not()){
+            primaryChildRepository.save(PrimaryChild(child.parent.id, child.id))
+        }
         childRepository.save(child)
     }
 }
