c9981018d1dfec88538f89dec8751ae1851a5651
Sim-km <simkm0824@naver.com>
2024-05-13 15:42:15 +0900

    feat: #50 자녀 조회시 대표 자녀 조회할 수 있는 메소드 추가

--- src/main/kotlin/com/asap/asapbackend/domain/child/domain/service/ChildReader.kt
+++ src/main/kotlin/com/asap/asapbackend/domain/child/domain/service/ChildReader.kt
@@ -3,14 +3,28 @@ package com.asap.asapbackend.domain.child.domain.service
 import com.asap.asapbackend.domain.child.domain.exception.ChildException
 import com.asap.asapbackend.domain.child.domain.model.Child
 import com.asap.asapbackend.domain.child.domain.repository.ChildRepository
+import com.asap.asapbackend.domain.child.domain.repository.PrimaryChildRepository
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 
 @Service
 class ChildReader(
-    private val childRepository: ChildRepository
+    private val childRepository: ChildRepository,
+    private val primaryChildRepository: PrimaryChildRepository
 ) {
     fun findAllByIds(childIds: Set<Long>): Set<Child> {
         return childRepository.findAllById(childIds).toSet()
+    }
+
+    fun findPrimaryChild(userId: Long): Child? {
+        return findChild {
+            primaryChildRepository.findByUserId(userId)?.let {
+                childRepository.findByIdOrNull(it.childId)
+            }
+        }
+    }
+
+    private fun findChild(function: () -> Child?): Child {
+        return function() ?: throw ChildException.ChildNotFoundException()
     }
 }
