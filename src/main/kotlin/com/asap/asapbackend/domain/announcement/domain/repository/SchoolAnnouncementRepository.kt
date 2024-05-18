248e4fd313c30fa865e04968232bc86fdac6b82c
Sim-km <simkm0824@naver.com>
2024-05-18 20:32:28 +0900

    fix: #69 학교 가정통신문 가장 최근 인덱스 값 조회 쿼리 수정

--- src/main/kotlin/com/asap/asapbackend/domain/announcement/domain/repository/SchoolAnnouncementRepository.kt
+++ src/main/kotlin/com/asap/asapbackend/domain/announcement/domain/repository/SchoolAnnouncementRepository.kt
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.Query
 interface SchoolAnnouncementRepository : JpaRepository<SchoolAnnouncement, Long> {
     @Query("""SELECT COALESCE(MAX(sa.idx), 0) 
         FROM SchoolAnnouncement sa
-         join SchoolAnnouncement.schoolAnnouncementPage as sap on sap.id = sa.schoolAnnouncementPage.id and sap.school.id = :schoolId
+         join sa.schoolAnnouncementPage as sap on sap.id = sa.schoolAnnouncementPage.id and sap.school.id = :schoolId
 """)
     fun findLastIndex(schoolId: Long): Int
 }
