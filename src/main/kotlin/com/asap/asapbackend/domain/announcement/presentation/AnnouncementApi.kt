f593a2c43e48f67ce8da98ff112b59cdf3cca601
Sim-km <simkm0824@naver.com>
2024-05-20 00:34:57 +0900

    docs: #75 관리자페이지에서 학교, 교육청 가정통신문 Id,제목, 카테고리 조회 api url 정의 및 명세 추가

--- src/main/kotlin/com/asap/asapbackend/domain/announcement/presentation/AnnouncementApi.kt
+++ src/main/kotlin/com/asap/asapbackend/domain/announcement/presentation/AnnouncementApi.kt
@@ -9,8 +9,10 @@ object AnnouncementApi {
         const val SCHOOL_ANNOUNCEMENT = "$BASE_URL/schools"
         const val SCHOOL_ANNOUNCEMENT_DETAIL = "$SCHOOL_ANNOUNCEMENT/{schoolAnnouncementId}"
         const val SCHOOL_ANNOUNCEMENT_CATEGORY = "$SCHOOL_ANNOUNCEMENT/{schoolAnnouncementId}/category"
+        const val SIMPLE_SCHOOL_ANNOUNCEMENT = "$SCHOOL_ANNOUNCEMENT/simple"
         const val EDUCATION_OFFICE_ANNOUNCEMENT = "$BASE_URL/education-offices"
         const val EDUCATION_OFFICE_ANNOUNCEMENT_DETAIL = "$EDUCATION_OFFICE_ANNOUNCEMENT/{educationOfficeAnnouncementId}"
         const val EDUCATION_OFFICE_ANNOUNCEMENT_CATEGORY = "$EDUCATION_OFFICE_ANNOUNCEMENT/{educationOfficeAnnouncementId}/category"
+        const val SIMPLE_EDUCATION_OFFICE_ANNOUNCEMENT = "$EDUCATION_OFFICE_ANNOUNCEMENT/simple"
     }
 }
