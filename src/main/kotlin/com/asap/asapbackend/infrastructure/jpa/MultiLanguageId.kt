b333b3d07b71e2a5222b1c1bd5df2a16e9614f20
Sim-km <simkm0824@naver.com>
2024-08-13 18:09:35 +0900

    feat: equals and hashcode 정의

--- src/main/kotlin/com/asap/asapbackend/infrastructure/jpa/MultiLanguageId.kt
+++ src/main/kotlin/com/asap/asapbackend/infrastructure/jpa/MultiLanguageId.kt
@@ -19,4 +19,25 @@ class MultiLanguageId(
         columnDefinition = "varchar(50)"
     )
     val language: Language = language
+
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as MultiLanguageId
+
+        if (id != other.id) return false
+        if (language != other.language) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id.hashCode()
+        result = 31 * result + language.hashCode()
+        return result
+    }
+
+
 }
